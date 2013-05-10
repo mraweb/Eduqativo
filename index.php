ec7dcf1ede220480787867771dd99d8312ec2650
Marcelo Almeida <marcelo@mraweb.com.br>
2013-05-10 14:06:52 -0300

    Atualizacao 10-05

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8" />
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<title>Eduqativo - Instituto Choque Cultural</title>
+<!--<link rel="stylesheet" type="text/css" href="css/style.css">-->
+<!--[if ie]>
+	<script type="text/javascript" src="js/html5-ie.js"></script>
+<![endif]-->
+<?php include "analytics.php"; ?>
+</head>
+<body itemscope itemtype="http://schema.org/WebPage">
+
+<div class="bg-home">
+    <header role="banner">
+        <h1><a href="/" title="Eduqativo - Instituto Choque Cultural">Eduqativo <span>Instituto Choque Cultural</span></a></h1>
+        
+        <nav role="navigation">
+            <ul>
+                <li><a href="/" title="Home" class="bt-home">Home</a></li>
+                <li><a href="quem-somos.php" title="Quem Somos" class="bt-quem">Quem Somos</a></li>
+                <li><a href="areas-de-atuacao.php" title="Áreas de Atuação" class="bt-area">Áreas de Atuação</a></li>
+                <li><a href="como-atuamos.php" title="Como Atuamos" class="bt-como">Como Atuamos</a></li>
+                <li><a href="parceiros.php" title="Parceiros" class="bt-parc">Parceiros</a></li>
+                <li><a href="participe.php" title="Participe" class="bt-part">Participe</a></li>
+                <li><a href="acontecendo.php" title="Acontecendo" class="bt-acon">Acontecendo</a></li>
+                <li><a href="conteudo.php" title="Conteúdo" class="bt-cont">Conteúdo</a></li>
+                <li><a href="fale-conosco.php" title="Fale Conosco" class="bt-fale">Fale Conosco</a></li>
+            </ul>
+        </nav>
+    </header><!-- FINAL HEADER -->
+    
+    <div role="main">
+        <section id="content">
+            <!-- NOTICIA REFERENTE A AREA DE ATUACAO - NOVOS METODOS -->
+            <section class="noticias">
+                <div class="laranja">
+                    <img src="" alt="Notícia Detalhe" width="" height="" />
+
+                    <h2><a href="noticia-detalhe.php" title="Loren Ipsum dolor it amet, consectour.">Loren Ipsum dolor it amet, consectour.</a></h2>
+                </div>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. [...]</p>
+            </section>
+
+
+            <!-- NOTICIA REFERENTE A AREA DE ATUACAO - LABORATORIO  -->
+            <section class="noticias">
+                <div class="lilas">
+                    <img src="" alt="Notícia Detalhe" width="" height="" />
+
+                    <h2><a href="noticia-detalhe.php" title="Loren Ipsum dolor it amet, consectour.">Loren Ipsum dolor it amet, consectour.</a></h2>
+                </div>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. [...]</p>
+            </section>
+
+
+            <!-- NOTICIA REFERENTE A AREA DE ATUACAO - INCUBADORA  -->
+            <section class="noticias">
+                <div class="verde">
+                    <img src="" alt="Notícia Detalhe" width="" height="" />
+
+                    <h2><a href="noticia-detalhe.php" title="Loren Ipsum dolor it amet, consectour.">Loren Ipsum dolor it amet, consectour.</a></h2>
+                </div>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. [...]</p>
+            </section>
+
+
+            <!-- NOTICIA REFERENTE A AREA DE ATUACAO - PRODUCAO DE CONTEUDO  -->
+            <section class="noticias sem-mrg-right">
+                <div class="pink">
+                    <img src="" alt="Notícia Detalhe" width="" height="" />
+
+                    <h2><a href="noticia-detalhe.php" title="Loren Ipsum dolor it amet, consectour.">Loren Ipsum dolor it amet, consectour.</a></h2>
+                </div>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. [...]</p>
+            </section>
+
+
+            <section class="box-areas">
+                <h2><a href="novos-metodos-de-educacao-atraves-da-arte.php" title="Novos Métodos de Educação Através da Arte" class="txt-laranja">Novos Métodos de Educação Através da Arte</a></h2>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. [...]</p>
+            </section>
+
+            <section class="box-areas">
+                <h2><a href="laboratorio-de-novas-praticas-urbanas.php" title="Laboratório de Novas Práticas Urbanas" class="txt-lilas">Laboratório de Novas Práticas Urbanas</a></h2>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. [...]</p>
+            </section>
+
+            <section class="box-areas">
+                <h2><a href="incubadora-de-artistas-e-projetos.php" title="Incubadora de Artistas e Projetos" class="txt-verde">Incubadora de Artistas e Projetos</a></h2>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. [...]</p>
+            </section>
+
+            <section class="box-areas">
+                <h2><a href="producao-de-conteudo.php" title="Produção de Conteúdo" class="txt-pink">Produção de Conteúdo</a></h2>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. [...]</p>
+            </section>
+
+            <div class="box-bottom">
+                <section class="parceiros">
+                    <h3>Parceiros</h3>
+
+                    <ul>
+                        <li>
+                            <h4>Financiadores</h4>
+
+                            <img src="" alt="" width="" height="" />
+                        </li>
+
+                        <li>
+                            <h4>Estratégicos</h4>
+
+                            <img src="" alt="" width="" height="" />
+                        </li>
+
+                        <li>
+                            <h4>Conteúdo</h4>
+
+                            <img src="" alt="" width="" height="" />
+                        </li>
+
+                        <li>
+                            <h4>Nossas Redes</h4>
+
+                            <img src="" alt="" width="" height="" />
+                        </li>
+                    </ul>
+                </section>
+
+                <section class="participe">
+                    <h3>Participe</h3>
+                    <h4>Seja Nosso Parceiro</h4>
+
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                </section>
+            </div>
+
+        </section><!-- FINAL CONTENT -->  
+    </div>
+</div>
+
+<div class="bg-rodape">
+    <footer role="contentinfo">
+        <nav role="navigation">
+            <ul>
+                <li><a href="/" title="Home" class="link">Home</a></li>
+                <li><a href="quem-somos.php" title="Quem Somos" class="link">Quem Somos</a></li>
+                <li><a href="areas-de-atuacao.php" title="Áreas de Atuação" class="link">Áreas de Atuação</a></li>
+                <li><a href="como-atuamos.php" title="Como Atuamos" class="link">Como Atuamos</a></li>
+                <li><a href="parceiros.php" title="Parceiros" class="link">Parceiros</a></li>
+                <li><a href="participe.php" title="Participe" class="link">Participe</a></li>
+                <li><a href="acontecendo.php" title="Acontecendo" class="link">Acontecendo</a></li>
+                <li><a href="conteudo.php" title="Conteúdo" class="link">Conteúdo</a></li>
+                <li><a href="fale-conosco.php" title="Fale Conosco" class="link">Fale Conosco</a></li>
+            </ul>
+        </nav>
+
+        <div class="face">
+
+        </div>
+
+        <section class="contatos">
+            <p><span>11</span> 2678-6600</p>
+
+            <p><a href="mailto:info@institutochoquecultural.org.br" title="E-mail" class="link">info@institutochoquecultural.org.br</a></p>
+        </section>
+
+        <p class="hcc"><a href="http://www.htmlcomcss.com.br" title="HTMLcomCSS - Criação de Sites" rel="external" class="hidetxt">HTMLcomCSS - Criação de Sites</a></p>
+    </footer>
+</div>
+<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+</body>
+</html>
